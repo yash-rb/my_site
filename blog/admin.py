86fda85677945973bcdf4e38a66027a1e892064f
Yash Rajbhar <yashraj78289@gmail.com>
2024-07-25 15:31:02 +0530

    Linked users to the posts, Created a custom admin panel and registered the blog app

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ['status', 'author', 'publish', 'created']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug':('title', )}
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,25 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Post(models.Model):
+    
+    class Status(models.TextChoices):
+        PUBLISHED = 'PB', 'Published'
+        DRAFT = 'DF', 'Draft'
+    
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    #Status of a post can be either be Draft--> DF or Published--> PB
+    status = models.CharField(max_length=2,choices=Status.choices, default=Status.DRAFT)
+    
+    """We use related_name to specify the name of the reverse relationship, from User to Post. This will
+    allow us to access related objects easily from a user object by using the user.blog_posts notation. """
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
 
     class Meta:
         # This ordering will apply by default for database queries when no specific order is provided in the query |   ##(-)hyphen indicates descending order before the field name, -publish.
