4b5f2f9f8a8fd28b0d9bb4ab0e9c079b0af1edc5
Yash Rajbhar <yashraj78289@gmail.com>
2024-07-26 11:56:46 +0530

    Added canonical seo friendly urls and a pagination mechanism to post_list

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 #Custom object manager
@@ -17,7 +18,7 @@ class Post(models.Model):
         DRAFT = 'DF', 'Draft'
     
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -44,3 +45,13 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", 
+                       args=[
+                           self.publish.year,
+                           self.publish.month,
+                           self.publish.day,
+                           self.slug
+                       ] )
+    
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
-
+from django.core.paginator import Paginator
 def post_list(request):
-    posts = Post.published.all()
+    
+    post_list = Post.published.all()
+    #Adding pagination to the post list view
+    paginator = Paginator(post_list, 2  )
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
+    
     return render(request, 'blog/post/list.html', {'posts':posts})
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
+    
     #This method will handle the error and generate a HTTP 404 (not found) exception if oject is not found" 
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
 
     return render(request, 'blog/post/detail.html', {'post':post}) 
     
